df8ea25959ba7d4f42ad0cf7f9420d0ee5f8bfa6
George Great <gosha8352@gmail.com>
2017-05-30 15:01:23 +0300

    Update Main.ml

--- Compiler design/Lab4/OCaml/Main.ml
+++ Compiler design/Lab4/OCaml/Main.ml
@@ -1,19 +1,12 @@
 open Batteries;;
 open DomainTag;;
 
-let strip_last_char str =
-    if str = "" then "" else
-    BatString.sub str 0 ((BatString.length str) - 1)
-
 let compiler = new Compiler.compiler;;
 let file = BatFile.open_in BatSys.argv.(1);;
 let program = ref (BatIO.read_all file);;
-program := strip_last_char !program;;
 BatIO.close_in file;;
 let scanner = new Scanner.scanner (BatUTF8.of_string_unsafe !program) compiler;;
 print_string "TOKENS\n";;
-(* let token = scanner#read_word scanner#cur true;;
-print_string (token#to_string ^ "\n"); *)
 
 let token = ref scanner#next_token;;
 while (!token#tag != End_of_program) do
@@ -26,4 +19,3 @@ if (compiler#errors) then begin
     print_string "MESSAGES\n";
     compiler#output_messages
 end;;
-
